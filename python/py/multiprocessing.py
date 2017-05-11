c1c46e33be74e1640e27ae06c254ef4ba2c09bcf
victor kinoti <victor kinoti>
2017-05-11 14:37:18 +0300

    multiprocessing assist in python

---
+++ python/py/multiprocessing.py
@@ -0,0 +1,8 @@
+import multiprocessing
+def spawn(num, num2):
+    print ('Spawned! {} {}'.format(num, num2))
+
+if __name__=='__main__':
+    for i in range(500):
+        p = multiprocessing.Process(target=spawn, args=(i, i+1))
+        p.start()

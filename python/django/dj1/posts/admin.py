87789937bd4097cd142c57223e5c656ae2a476f5
victor kinoti <victor kinoti>
2017-06-22 14:06:07 +0300

    django project1

--- python/django/dj1/posts/admin.py
+++ python/django/dj1/posts/admin.py
@@ -1,4 +1,12 @@
 from django.contrib import admin
 from .models import Post
 
-admin.site.register(Post)
+class PostModelAdmin(admin.ModelAdmin):
+    list_display = ["title", "timestamp", "updated"]
+    list_display_links = ['updated', 'title']
+    list_filter = ['updated', 'timestamp']
+    search_fields = ['title', 'content']
+    class Meta:
+        model = Post
+  
+admin.site.register(Post, PostModelAdmin)
